853cb1df0a8f100a762f0d6ab54f975f08f0c5c0
Liam Collod <lcollod@gmail.com>
2023-11-29 19:48:42 +0100

    feat(git): first function get_current_commit_hash

---
+++ python/pythonning/git.py
@@ -0,0 +1,40 @@
+"""
+perhaps worth to create its own package for it, but good here as long as it doesn't
+grow too big.
+
+Of course all the commands assume git is installed on the system and can be accessed
+by calling ``git`` in the terminal.
+"""
+import logging
+import subprocess
+from pathlib import Path
+
+LOGGER = logging.getLogger(__name__)
+
+
+def _call_git(git_args: list[str], cwd: Path = None) -> str:
+    """
+    Call a git command in the terminal and return its output as a decoded string.
+
+    Args:
+        git_args: list of argument to pass to git.
+        cwd: current working directory, usually root of the git repository.
+
+    Returns:
+        output of the git command
+    """
+    command = ["git"] + git_args
+    commit = subprocess.check_output(command, cwd=cwd)
+    return commit.decode("utf-8")
+
+
+def get_current_commit_hash(repository_path: Path = None) -> str:
+    """
+    Return the hash of the latest commit the repository is currently at.
+
+    Args:
+        repository_path:
+            optional filesysten path to an existing directory,
+            use current working directory (cwd) if not provided.
+    """
+    return _call_git(["rev-parse", "HEAD"], cwd=repository_path)
