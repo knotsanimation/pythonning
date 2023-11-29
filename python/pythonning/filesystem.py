082e6c6fe7f256f82159dce736d5c8d85aa1adab
Liam Collod <lcollod@gmail.com>
2023-11-29 19:13:57 +0100

    feat(filesystem): add extract_zip

--- python/pythonning/filesystem.py
+++ python/pythonning/filesystem.py
@@ -2,6 +2,7 @@ import logging
 import os
 import shutil
 import stat
+import zipfile
 from pathlib import Path
 
 LOGGER = logging.getLogger(__name__)
@@ -45,3 +46,24 @@ def copy_path_to(path: Path, target_path: Path):
             path,
             target_path,
         )
+
+
+def extract_zip(zip_path: Path, remove_zip=True):
+    """
+    Exract the given zip archive content in the directory it is in.
+
+    Args:
+        zip_path: path to an existing zip file on the filesystem.
+        remove_zip: True to delete the zip once extracted
+
+    Returns:
+        root directory the extracted file can be found at
+    """
+    extract_root = zip_path.parent
+    with zipfile.ZipFile(zip_path, "r") as zip_file:
+        zip_file.extractall(extract_root)
+
+    if remove_zip:
+        zip_path.unlink()
+
+    return extract_root
