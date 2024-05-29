d9d5610ba6aa07235f921c28d9855288a88ba23c
Liam Collod <lcollod@gmail.com>
2024-05-29 18:58:10 +0200

    chore: upgrade version to 1.8.0

--- package.py
+++ package.py
@@ -2,7 +2,7 @@
 
 name = "pythonning"
 
-version = "1.7.0"
+version = "1.8.0"
 
 authors = ["Liam Collod"]
 
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ long_description = (THISDIR / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="pythonning",
-    version="1.7.0",
+    version="1.8.0",
     description="Low-level python functions commonly used by any kind of developer.",  # Optional
     long_description=long_description,  # Optional
     long_description_content_type="text/markdown",  # Optional
